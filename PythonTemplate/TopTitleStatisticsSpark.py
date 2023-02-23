086df9b403d0b5894946fefb7707abd71e997807
Jingtian Yao <jy2867@columbia.edu>
2023-02-22 22:15:55 -0800

    Part B

--- PythonTemplate/TopTitleStatisticsSpark.py
+++ PythonTemplate/TopTitleStatisticsSpark.py
@@ -8,7 +8,25 @@ sc = SparkContext(conf=conf)
 
 lines = sc.textFile(sys.argv[1], 1)
 
+
 #TODO
+lines_list = lines.collect()
+counts = []
+
+for line in lines_list:
+    # TODO
+    line = line.strip()
+    _, count = line.split('\t')
+    counts.append(int(count))
+
+sum_val = int(sum(counts))
+mean_val = int(sum_val/len(counts))
+min_val = int(min(counts))
+max_val = int(max(counts))
+
+temp_lst = [pow(x-mean_val, 2) for x in counts]
+var_val = int(sum(temp_lst)/len(counts))
+
 
 outputFile = open(sys.argv[2], "w")
 '''
@@ -20,6 +38,12 @@ outputFile.write('Min\t%s\n' % ans3)
 outputFile.write('Max\t%s\n' % ans4)
 outputFile.write('Var\t%s\n' % ans5)
 '''
+outputFile.write('Mean\t%s\n' % mean_val)
+outputFile.write('Sum\t%s\n' % sum_val)
+outputFile.write('Min\t%s\n' % min_val)
+outputFile.write('Max\t%s\n' % max_val)
+outputFile.write('Var\t%s\n' % var_val)
 
+outputFile.close()
 sc.stop()
 
