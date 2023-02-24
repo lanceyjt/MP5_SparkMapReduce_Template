d64271ea84c421b5d602f3802b8014c9c7627cf5
Jingtian Yao <jy2867@columbia.edu>
2023-02-23 21:07:05 -0800

    Part D

--- PythonTemplate/TopPopularLinksSpark.py
+++ PythonTemplate/TopPopularLinksSpark.py
@@ -6,14 +6,32 @@ conf = SparkConf().setMaster("local").setAppName("TopPopularLinks")
 conf.set("spark.driver.bindAddress", "127.0.0.1")
 sc = SparkContext(conf=conf)
 
-lines = sc.textFile(sys.argv[1], 1) 
+lines = sc.textFile(sys.argv[1], 100) 
 
 #TODO
+lines_lst = lines.collect()
+
+page_freq = {}
+for line in lines_lst:
+  #TODO
+  line = line.strip()
+  _, dest = line.split(":")
+  dests = [x.strip() for x in dest.split(' ') if x.strip()!='']
+  for d in dests:
+    page_freq[d] = page_freq.get(d,0)+1
+
+page_freq_lst = list(page_freq.items())
+page_freq_lst.sort(reverse=False, key=lambda x: (x[1], x[0]))
+
+output_lines = ["\t".join(x) for x in page_freq_lst[-10:]]
+result = "\n".join(output_lines)
 
 output = open(sys.argv[2], "w")
 
 #TODO
 #write results to output file. Foramt for each line: (key + \t + value +"\n")
+outputFile.write(result)
+outputFile.close()
 
 sc.stop()
 
