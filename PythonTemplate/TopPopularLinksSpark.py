84ff1f15b04e0cc3ed9175357522693eef48f903
Jingtian Yao <jy2867@columbia.edu>
2023-02-23 21:09:57 -0800

    bugfix

--- PythonTemplate/TopPopularLinksSpark.py
+++ PythonTemplate/TopPopularLinksSpark.py
@@ -23,15 +23,15 @@ for line in lines_lst:
 page_freq_lst = list(page_freq.items())
 page_freq_lst.sort(reverse=False, key=lambda x: (x[1], x[0]))
 
-output_lines = ["\t".join(x) for x in page_freq_lst[-10:]]
+output_lines = ["{page}\t{count}".format(page=x[0], count=x[1]) for x in page_freq_lst[-10:]]
 result = "\n".join(output_lines)
 
 output = open(sys.argv[2], "w")
 
 #TODO
 #write results to output file. Foramt for each line: (key + \t + value +"\n")
-outputFile.write(result)
-outputFile.close()
+output.write(result)
+output.close()
 
 sc.stop()
 
